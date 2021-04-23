bee0f5a233a301300cdd1dc2bb79aa98f677fc4a
gaku <pro.gaku@gmail.com>
2021-04-24 07:48:48 +0900

    statenotifierを導入

--- lib/page/init/init_page.dart
+++ lib/page/init/init_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_null_safety/page/init/init_page_controller.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:provider/provider.dart';
 
 class InitPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<InitPageController>(
+    return StateNotifierProvider<InitPageController, InitPageState>(
       create: (_) => InitPageController(),
       builder: (BuildContext context, Widget? _) {
         return Scaffold(
@@ -13,18 +14,62 @@ class InitPage extends StatelessWidget {
             child: Column(
               children: [
                 Text('init page'),
-                Text(
-                  context.watch<InitPageController>().counter.toString(),
-                ),
-                OutlinedButton(
-                  child: Text('countup'),
-                  onPressed: () => context.read<InitPageController>().increment(),
-                ),
+                CounterWidget(),
+                ErrorWidget(),
               ],
             ),
           ),
         );
       },
+    );
+  }
+}
+
+class ErrorWidget extends StatelessWidget {
+  const ErrorWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          context.watch<InitPageState>().error.toString(),
+        ),
+        Text(
+          context.watch<InitPageState>().isError.toString(),
+        ),
+        OutlinedButton(
+          child: Text('error'),
+          onPressed: () => context.read<InitPageController>().error(),
+        ),
+        OutlinedButton(
+          child: Text('clear error'),
+          onPressed: () => context.read<InitPageController>().clearError(),
+        ),
+      ],
+    );
+  }
+}
+
+class CounterWidget extends StatelessWidget {
+  const CounterWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          context.watch<InitPageState>().counter.toString(),
+        ),
+        OutlinedButton(
+          child: Text('count up'),
+          onPressed: () => context.read<InitPageController>().increment(),
+        ),
+      ],
     );
   }
 }
--- lib/page/init/init_page_controller.dart
+++ lib/page/init/init_page_controller.dart
@@ -1,10 +1,36 @@
-import 'package:flutter/cupertino.dart';
+import 'package:state_notifier/state_notifier.dart';
 
-class InitPageController with ChangeNotifier {
-  int counter = 0;
+class InitPageState {
+  InitPageState({
+    this.counter = 0,
+    this.error = "",
+    this.isError = false,
+  });
+  int counter;
+  String error;
+  bool isError;
+
+  InitPageState copyWith({int? counter, String? error, bool? isError}) {
+    return InitPageState(
+      counter: counter ?? this.counter,
+      error: error ?? this.error,
+      isError: isError ?? this.isError,
+    );
+  }
+}
+
+class InitPageController extends StateNotifier<InitPageState> {
+  InitPageController() : super(InitPageState());
 
   void increment() {
-    this.counter++;
-    notifyListeners();
+    state = state.copyWith(counter: state.counter + 1, error: null);
+  }
+
+  void error() {
+    state = state.copyWith(error: "error!", isError: true);
+  }
+
+  void clearError() {
+    state = state.copyWith(error: "", isError: false);
   }
 }
