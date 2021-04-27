bc9bb03b710347a26703210683174bfb791ac25b
gaku <pro.gaku@gmail.com>
2021-04-28 06:18:47 +0900

    ripositoryパターン導入

---
+++ lib/repository/post_repository/post_repository.freezed.dart
@@ -0,0 +1,155 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'post_repository.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Post _$PostFromJson(Map<String, dynamic> json) {
+  return _Post.fromJson(json);
+}
+
+/// @nodoc
+class _$PostTearOff {
+  const _$PostTearOff();
+
+  _Post call({String accountName = ''}) {
+    return _Post(
+      accountName: accountName,
+    );
+  }
+
+  Post fromJson(Map<String, Object> json) {
+    return Post.fromJson(json);
+  }
+}
+
+/// @nodoc
+const $Post = _$PostTearOff();
+
+/// @nodoc
+mixin _$Post {
+  String get accountName => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $PostCopyWith<Post> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $PostCopyWith<$Res> {
+  factory $PostCopyWith(Post value, $Res Function(Post) then) =
+      _$PostCopyWithImpl<$Res>;
+  $Res call({String accountName});
+}
+
+/// @nodoc
+class _$PostCopyWithImpl<$Res> implements $PostCopyWith<$Res> {
+  _$PostCopyWithImpl(this._value, this._then);
+
+  final Post _value;
+  // ignore: unused_field
+  final $Res Function(Post) _then;
+
+  @override
+  $Res call({
+    Object? accountName = freezed,
+  }) {
+    return _then(_value.copyWith(
+      accountName: accountName == freezed
+          ? _value.accountName
+          : accountName // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$PostCopyWith<$Res> implements $PostCopyWith<$Res> {
+  factory _$PostCopyWith(_Post value, $Res Function(_Post) then) =
+      __$PostCopyWithImpl<$Res>;
+  @override
+  $Res call({String accountName});
+}
+
+/// @nodoc
+class __$PostCopyWithImpl<$Res> extends _$PostCopyWithImpl<$Res>
+    implements _$PostCopyWith<$Res> {
+  __$PostCopyWithImpl(_Post _value, $Res Function(_Post) _then)
+      : super(_value, (v) => _then(v as _Post));
+
+  @override
+  _Post get _value => super._value as _Post;
+
+  @override
+  $Res call({
+    Object? accountName = freezed,
+  }) {
+    return _then(_Post(
+      accountName: accountName == freezed
+          ? _value.accountName
+          : accountName // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Post implements _Post {
+  const _$_Post({this.accountName = ''});
+
+  factory _$_Post.fromJson(Map<String, dynamic> json) =>
+      _$_$_PostFromJson(json);
+
+  @JsonKey(defaultValue: '')
+  @override
+  final String accountName;
+
+  @override
+  String toString() {
+    return 'Post(accountName: $accountName)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _Post &&
+            (identical(other.accountName, accountName) ||
+                const DeepCollectionEquality()
+                    .equals(other.accountName, accountName)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^ const DeepCollectionEquality().hash(accountName);
+
+  @JsonKey(ignore: true)
+  @override
+  _$PostCopyWith<_Post> get copyWith =>
+      __$PostCopyWithImpl<_Post>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$_$_PostToJson(this);
+  }
+}
+
+abstract class _Post implements Post {
+  const factory _Post({String accountName}) = _$_Post;
+
+  factory _Post.fromJson(Map<String, dynamic> json) = _$_Post.fromJson;
+
+  @override
+  String get accountName => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$PostCopyWith<_Post> get copyWith => throw _privateConstructorUsedError;
+}
---
+++ lib/repository/post_repository/post_repository.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_repository.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Post _$_$_PostFromJson(Map<String, dynamic> json) {
+  return _$_Post(
+    accountName: json['accountName'] as String? ?? '',
+  );
+}
+
+Map<String, dynamic> _$_$_PostToJson(_$_Post instance) => <String, dynamic>{
+      'accountName': instance.accountName,
+    };
